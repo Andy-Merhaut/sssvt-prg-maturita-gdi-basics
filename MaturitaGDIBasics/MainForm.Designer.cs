90cf5c5df5a937a7980c462814f79a4a1eea9125
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-16 15:56:18 +0200

    Přidejte soubory projektu.

---
+++ MaturitaGDIBasics/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace MaturitaGDIBasics
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(800, 450);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            this.MainPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPictureBox_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPictureBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+    }
+}
+
---
+++ MaturitaGDIBasics/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MaturitaGDIBasics
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            /*pocatek, odkud mi zacina vykreslovani LAJNY*/
+            /*definice    =    souradnice(X, Y)*/
+            Point pocatek = new Point(100, 50);
+            Point konec = new Point(200, 50);
+
+            Pen blackPen = new Pen(Color.Black);
+            Font font = new Font("Arial", 20);
+            SolidBrush brush = new SolidBrush(Color.DeepPink);
+
+            e.Graphics.DrawLine(blackPen, pocatek, konec);
+
+            e.Graphics.DrawRectangle(blackPen, pocatek.X + 20, pocatek.Y + 20, 75, 50);
+
+            e.Graphics.DrawString("TEXT", font, brush, pocatek.X + 20 + 5, pocatek.Y + 20 + 5);
+        }
+    }
+}
